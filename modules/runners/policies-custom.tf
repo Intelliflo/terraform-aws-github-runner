be191b014a9a0860a62605dcc58405faff8bd6e2
Pedro Brasil Borges <pedrobrasilborges@gmail.com>
2025-05-05 12:27:39 +0100

    fix: remove duplicated entries

--- modules/runners/policies-custom.tf
+++ modules/runners/policies-custom.tf
@@ -21,8 +21,6 @@ resource "aws_iam_role_policy" "gh_artifacts_bucket" {
           "arn:aws:s3:::dev-github-ci-loop-artifacts",
           "arn:aws:s3:::github-ci-loop-artifacts/*",
           "arn:aws:s3:::github-ci-loop-artifacts",
-          "arn:aws:s3:::dev-github-ci-loop-artifacts/*",
-          "arn:aws:s3:::dev-github-ci-loop-artifacts",
           "arn:aws:s3:::packages.shs-ie-01.intelliflo.services/*",
           "arn:aws:s3:::packages.shs-ie-01.intelliflo.services",
           "arn:aws:s3:::mssql-migrations.shs-ie-01.intelliflo.services/*",
